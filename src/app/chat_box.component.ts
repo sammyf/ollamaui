525d36994b3df9a0e4f0bc3bbfac625a980fb6e7
Benjamin Speidel <benjamin.speidel@smartinsurtech.de>
2024-07-30 14:28:02 +0200

    add jest

--- src/app/chat_box.component.ts
+++ src/app/chat_box.component.ts
@@ -1,18 +1,16 @@
-import {Component, Input} from '@angular/core';
-import {Messages} from "../models/ollama";
-import {CommonModule} from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Messages } from '../models/ollama.models';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'chat_box',
   templateUrl: './chat_box.component.html',
   styleUrls: ['./chat_box.component.css'],
-  imports: [
-  ],
-  standalone: true
+  imports: [],
+  standalone: true,
 })
 export class ChatBoxComponent {
   @Input() content!: string;
   @Input() role!: string;
   @Input() persona!: string;
 }
-
